ee0718708937694baae78f71dbbaeed65fb231ab
Bibingsu <vince_arabe@hotmail.com>
2025-10-16 15:19:22 +0800

    Update button-1.php

--- backend/app/Views/components/button-1.php
+++ backend/app/Views/components/button-1.php
@@ -1,4 +1,4 @@
-<a href="#menu" 
-   class="inline-block bg-yellow-700 text-white px-6 py-3 rounded-full font-semibold shadow-md hover:bg-yellow-600 hover:scale-105 transition transform duration-200">
-   Explore Menu
+<a href="#menu"
+   class="inline-block bg-yellow-700 hover:bg-yellow-600 shadow-md px-6 py-3 rounded-full font-semibold text-white hover:scale-105 transition duration-200 transform">
+   <?= esc($label ?? "no label yet") ?>
 </a>
